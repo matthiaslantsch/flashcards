14d5c89ad208e8b86535b7dbb7387996d9d9e6f1
Matthias Lantsch <matthias.lantsch@bluewin.ch>
2018-12-21 23:10:49 +0100

    Created empty new project structure to migrate the code into

---
+++ config/flashcards.conf.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the holonet flashcards application
+ * (c) Matthias Lantsch
+ *
+ * config file for project specific config options
+ * the project specific config will override these values if set there
+ *
+ * @package flashcards
+ * @license http://www.wtfpl.net/ Do what the fuck you want Public License
+ * @author  Matthias Lantsch <matthias.lantsch@bluewin.ch>
+ */
+
+ /**
+  * Database conntection configuration
+  */
+ $config["db"] = array(
+ 	/**
+ 	 * the pdo driver to be used for the database (need to have the driver+activerecord crud helpers installed)
+ 	 */
+ 	"driver" => "sqlite",
+ 	/**
+ 	 * The file of the sqlite database
+ 	 */
+ 	"file" => "%app.vardir%{app}.db"
+ );
---
+++ config/flashcards.routes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* This file is part of the holonet flashcards application
+ * (c) Matthias Lantsch
+ *
+ * php route definition file
+ *
+ * @package flashcards
+ * @license http://www.wtfpl.net/ Do what the fuck you want Public License
+ * @author  Matthias Lantsch <matthias.lantsch@bluewin.ch>
+ */
+
+use holonet\holofw\FWRouter;
